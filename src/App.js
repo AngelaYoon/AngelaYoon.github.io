48de570400a1f6a0385b89eec8439f96fa973a71
angelayoon <angela.h.yoon@gmail.com>
2021-08-24 02:10:53 -0500

    deploy blog as github pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom'
 
-const getPosts =() => 
-  fetch('https://dev.to/api/articles?username=angelayoon').then((res) =>
-  res.json()
+const getPosts = () => 
+  fetch("https://dev.to/api/articles?username=angelayoon").then((res) =>
+    res.json()
+  )
+
+const getPost = (id) => 
+  fetch(`https://dev.to/api/articles${id}`).then((res) => 
+    res.json()
   )
 
 const Post = () => {
-  return <>post</>
+  const { id } = useParams()
+
+  const [post, setPost] = React.useState([])
+
+  React.useEffect(() => {
+    getPost(id).then((data) => setPost(data))
+  },[])
+  
+  return <>{post.body_html}</>
 }
 
-const Posts =() => {
+const Posts = () => {
   const [posts, setPosts] = React.useState([])
 
   React.useEffect(() => {
-    getPosts().then(data => setPosts(data))
+    getPosts().then((data) => setPosts(data))
   }, [])
 
   return(
     <>
-      {posts.map((posts) => (
-        <a
-        className="App-link"
-        href={posts.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          {posts.title}
-        </a>
+      {posts.map((post) => (
+        <Link className="App-link" to={`posts/${post.id}`}>
+          {post.title}
+        </Link>
       ))}
     </>
   )
@@ -39,16 +53,18 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src="https://media-exp1.licdn.com/dms/image/C5603AQG6KeNY5pMjsQ/profile-displayphoto-shrink_200_200/0/1628085038443?e=1633564800&v=beta&t=-9BNiS-2sg-4J4XiJNFTBk21RvDaY_WO3QdC4BT4tUM" className="App-logo" alt="logo" />
-          <p>
-            Angela Yoon
-          </p>
+          <img 
+            src="https://media-exp1.licdn.com/dms/image/C5603AQG6KeNY5pMjsQ/profile-displayphoto-shrink_200_200/0/1628085038443?e=1633564800&v=beta&t=-9BNiS-2sg-4J4XiJNFTBk21RvDaY_WO3QdC4BT4tUM" 
+            className="App-logo" 
+            alt="logo"
+          />
+          <p>Angela Yoon</p>
           <Switch>
             <Route exact path="/">
               <Posts />
             </Route>
-            <Route exact path="/posts/id">
-              <Posts />
+            <Route exact path="/posts/:id">
+              <Post />
             </Route>
           </Switch>
           
